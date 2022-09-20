9b74a98bdb9d49f5f6fe8285fe58b2fc1e3e3024
Xenox003 <xenox003yt@gmail.com>
2022-09-20 21:08:37 +0200

    UI Update

--- .compileui/run.sh
+++ .compileui/run.sh
@@ -4,7 +4,7 @@ for file in ./.compileui/*.ui; do
     pureFileName=${file##*/}
 
     echo $PWD
-    pyuic5 "$file" -o "app/modules/ui/${pureFileName//.ui/}_ui.py"
+    pyuic5 "$file" -o "app/modules/ui/compiled/ui_${pureFileName//.ui/}.py"
 done
 
 git add *
