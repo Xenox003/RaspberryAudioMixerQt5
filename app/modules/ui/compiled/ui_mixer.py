fa143c60a3bfe32bb4733168c12fd35123177f13
Xenox003 <xenox003yt@gmail.com>
2022-09-21 18:06:18 +0200

    Auto: UI Update

--- app/modules/ui/compiled/ui_mixer.py
+++ app/modules/ui/compiled/ui_mixer.py
@@ -14,19 +14,10 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MixerWidget(object):
     def setupUi(self, MixerWidget):
         MixerWidget.setObjectName("MixerWidget")
-        MixerWidget.resize(100, 480)
-        self.pushButton = QtWidgets.QPushButton(MixerWidget)
-        self.pushButton.setGeometry(QtCore.QRect(10, 440, 80, 24))
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(MixerWidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(10, 400, 80, 24))
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.pushButton_3 = QtWidgets.QPushButton(MixerWidget)
-        self.pushButton_3.setGeometry(QtCore.QRect(10, 360, 80, 24))
-        self.pushButton_3.setObjectName("pushButton_3")
-        self.pushButton_4 = QtWidgets.QPushButton(MixerWidget)
-        self.pushButton_4.setGeometry(QtCore.QRect(10, 320, 80, 24))
-        self.pushButton_4.setObjectName("pushButton_4")
+        MixerWidget.resize(100, 430)
+        self.widget = Ui_VUMeterWidget(MixerWidget)
+        self.widget.setGeometry(QtCore.QRect(0, 0, 50, 430))
+        self.widget.setObjectName("widget")
 
         self.retranslateUi(MixerWidget)
         QtCore.QMetaObject.connectSlotsByName(MixerWidget)
@@ -34,7 +25,4 @@ class Ui_MixerWidget(object):
     def retranslateUi(self, MixerWidget):
         _translate = QtCore.QCoreApplication.translate
         MixerWidget.setWindowTitle(_translate("MixerWidget", "Form"))
-        self.pushButton.setText(_translate("MixerWidget", "PushButton"))
-        self.pushButton_2.setText(_translate("MixerWidget", "PushButton"))
-        self.pushButton_3.setText(_translate("MixerWidget", "PushButton"))
-        self.pushButton_4.setText(_translate("MixerWidget", "PushButton"))
+from ui_mixer import Ui_VUMeterWidget
